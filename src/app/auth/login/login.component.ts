c1442909336470158d684cc5426b86cbe2b30709
Mandraszewsky <mandrah@o2.pl>
2025-01-27 13:30:23 +0100

    custom & async validators

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -12,23 +13,42 @@ export class LoginComponent {
   // form setup:
   form = new FormGroup({
     email: new FormControl('', {
-      validators: [Validators.email, Validators.required]
+      validators: [Validators.email, Validators.required],
+      asyncValidators: [emailIsUnique]
     }),
     password: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6)]
+      validators: [Validators.required, Validators.minLength(6), mustContainQuestionMark]
     })
   });
 
-  get emailIsInvalid(){
+  get emailIsInvalid() {
     return (this.form.controls.email.touched && this.form.controls.email.dirty && this.form.controls.email.invalid);
   }
 
-  get passwordlIsInvalid(){
+  get passwordlIsInvalid() {
     return (this.form.controls.password.touched && this.form.controls.password.dirty && this.form.controls.password.invalid);
   }
 
-  onSubmit(){
+  onSubmit() {
     const enteredEmail = this.form.value.email;
     const enteredPassword = this.form.value.password;
   }
+}
+
+//dummy validator as an example for custom validator:
+function mustContainQuestionMark(control: AbstractControl) {
+  if (control.value.includes('?')) {
+    return null;
+  }
+
+  return { doesNotContainQuestionMark: true };
+}
+
+//dummy async validator as an example for custom validator, observable with async for example should send http request:
+function emailIsUnique(control: AbstractControl) {
+  if (control.value !== 'test@example.com') {
+    return of(null); // of - produce an observable that emits a value, in this case = null
+  }
+
+  return of({ notUnique: true });
 }
